48184142800504dce5887abe29a4a895d289e96d
KevinJeanMarie <kjeanmarie01@gmail.com>
2023-04-05 16:47:20 +0200

    in progress

--- app/src/main/java/com/example/android/MainActivity.kt
+++ app/src/main/java/com/example/android/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.android
 
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.SpannableStringBuilder
+import android.text.style.StyleSpan
+import android.widget.TextView
 import android.widget.Toast
 import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
@@ -9,19 +13,31 @@ import com.bumptech.glide.Glide
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.details )
+        setContentView(R.layout.details)
         supportActionBar?.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.toolbar))
 
-        val text = "Hello toast!"
-        val duration = Toast.LENGTH_LONG
+        findViewById<TextView>(R.id.barcode).applyBoldText("Code-barres", "UYGUGU")
 
-        val toast = Toast.makeText(applicationContext, text, duration)
-        toast.show()
+        fun toast(){
+            val text = "Hello toast!"
+            val duration = Toast.LENGTH_LONG
+
+            val toast = Toast.makeText(applicationContext, text, duration)
+            toast.show()
+        }
+
+        toast()
+    }
+
+    fun TextView.applyBoldText(prefix: String, value: String) {
+        val text = "$prefix : $value"
+        val builder = SpannableStringBuilder(text)
+        builder.setSpan(StyleSpan(Typeface.BOLD), 0, text.indexOf(":") + 1, 0)
+        setText(text)
 
     }
 }
-
-class Product : AppCompatActivity() {
+class Product : AppCompatActivity(){
     val name: String,
     val brand: String,
     val barcode: String,
@@ -31,5 +47,7 @@ class Product : AppCompatActivity() {
     val countries: List<String>,
     val ingredients: List<String>,
     val allergens: List<String>,
-    val additives: List<String>,
+    val additives: List<String>
 }
+
+
